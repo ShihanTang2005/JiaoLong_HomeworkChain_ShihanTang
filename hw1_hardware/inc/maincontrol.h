8a0503c50d54f8ec5b4e6b765686cc39157e76a0
TSH <tangyong72@163.com>
2023-12-22 14:55:27 +0800

    feat: 新增can通道，新yaw和pitch电机初始化

---
+++ hw1_hardware/inc/maincontrol.h
@@ -0,0 +1,12 @@
+//
+// Created by TSH-STUDIO 唐诗涵 on 2023/12/22.
+//
+
+#ifndef HW1_HARDWARE_MAINCONTROL_H
+#define HW1_HARDWARE_MAINCONTROL_H
+#include "motor.h"
+
+extern Motor GMY;
+extern Motor GMP;
+
+#endif //HW1_HARDWARE_MAINCONTROL_H
